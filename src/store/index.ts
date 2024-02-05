5100bc691b9bb2256b6eb1f77d3b0d9dff547b7f
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-06 04:05:36 +0900

    feat: 난이도 데이터 저장 기능 구현

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,35 +1,36 @@
 import { configureStore } from "@reduxjs/toolkit";
-import // persistStore,
-// persistReducer,
-// FLUSH,
-// REHYDRATE,
-// PAUSE,
-// PERSIST,
-// PURGE,
-// REGISTER,
-"redux-persist";
-// import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./reducers";
 
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-// };
+const persistConfig = {
+  key: "root",
+  storage,
+};
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: rootReducer,
-  // middleware: (getDefaultMiddleware) =>
-  //   getDefaultMiddleware({
-  //     serializableCheck: {
-  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-  //     },
-  //   }),
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-// export const persistor = persistStore(store);
+export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
 
