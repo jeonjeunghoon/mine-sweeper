517de54f7472a6fb6b9b450263833cc659055918
jeonjeunghoon <jeonjeunghoon@gmail.com>
2024-02-05 14:19:37 +0900

    feat: `Layout` 컴포넌트 구현

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,16 @@
+import { PropsWithChildren } from "react";
+
+import styled from "@emotion/styled";
+
+export default function Layout({ children }: PropsWithChildren) {
+  return <S.Layout>{children}</S.Layout>;
+}
+
+const S = {
+  Layout: styled.div`
+    height: 100%;
+
+    overflow-x: auto;
+    background-color: black;
+  `,
+};
